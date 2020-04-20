446402d39342dfb6431c19bcbfbb562a27e950f6
LEONEL BAUDRIT <you@example.comleonel.baudrit@gmail.com>
2020-04-19 20:43:55 -0500

    Adding PyPoll.py file.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -8,9 +8,9 @@
 import os
 import csv
 #Assign a variable to load a file from a path
-file_to_load = os.path.join('.', "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join('.', "analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initialize a total vote counter.
 total_votes = 0
 # Candidate options and candidate votes
